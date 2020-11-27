79e0b14f2586da5d005ad3449533cd53dc00b6b5
jrpespinas <jrpespinas@gmail.com>
2020-11-27 20:25:37 +0800

    chore: change to post method

--- main.py
+++ main.py
@@ -9,10 +9,6 @@ def read_main():
     return {"message": "Hello World"}
 
 
-@app.get('/article/{article_id}')
-def analyze_article(article_id: int, q: str = None):
-    count = 0
-    if q:
-        doc = nlp(q)
-        count = len(doc.ents)
-    return {"article_id": article_id, "q": q, "count": count}
+@app.post('/article/')
+def analyze_article(): 
+    return {} 
