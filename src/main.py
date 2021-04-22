b51bd09fda6b1e14e945c9ca8a75d75b8325157c
jrpespinas <jrpespinas@gmail.com>
2021-04-22 14:40:03 +0800

    feat: add get by id method

--- src/main.py
+++ src/main.py
@@ -29,3 +29,9 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
+@app.get("/blog/{id}")
+def get_blog_by_id(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
