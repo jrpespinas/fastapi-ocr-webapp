893d0107e4ef27ce50a4a29debb9d003c76974d8
jrpespinas <jrpespinas@gmail.com>
2021-04-23 13:05:43 +0800

    feat: add delete method

--- src/main.py
+++ src/main.py
@@ -25,6 +25,15 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id)\
+        .delete(synchronize_session=False)
+    db.commit()
+
+    return {"message": "Blog deleted!"}
+
+
 @app.get("/blog")
 def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
