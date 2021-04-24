f523b95a4b9bfbcfdb37a5a558550c0cb78d6cf8
jrpespinas <jrpespinas@gmail.com>
2021-04-24 14:51:43 +0800

    feat: implement exception response 404

--- src/main.py
+++ src/main.py
@@ -43,7 +43,16 @@ def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}")
 def destroy(id: int, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with the id {id} is not available!",
+        )
+    else:
+        blog.delete(synchronize_session=False)
+
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -57,6 +66,7 @@ def get_blogs(db: Session = Depends(get_db)):
 @app.get("/blog/{id}", status_code=status.HTTP_200_OK)
 def get_blog_by_id(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
     if not blog:
         raise HTTPException(
             status_code=status.HTTP_201_CREATED,
