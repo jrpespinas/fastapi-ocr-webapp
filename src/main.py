601764648deeb2b5cf022f34d3e8b054d240a8fd
jrpespinas <jrpespinas@gmail.com>
2021-04-24 14:06:11 +0800

    feat: add exception if blog does not exist

--- src/main.py
+++ src/main.py
@@ -27,9 +27,16 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(
-        {"title": request.title, "body": request.body}
-    )
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with the id {id} is not available!",
+        )
+    else:
+        blog.update({"title": request.title, "body": request.body})
+
     db.commit()
     return {"message": "Updated"}
 
@@ -48,7 +55,7 @@ def get_blogs(db: Session = Depends(get_db)):
 
 
 @app.get("/blog/{id}", status_code=status.HTTP_200_OK)
-def get_blog_by_id(id: int, response: Response, db: Session = Depends(get_db)):
+def get_blog_by_id(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(
