35190f259a2c08df616a059c85ed001cccc2e6c1
jrpespinas <jrpespinas@gmail.com>
2021-04-27 12:00:05 +0800

    feat: add post user method

--- src/main.py
+++ src/main.py
@@ -74,3 +74,14 @@ def get_blog_by_id(id: int, db: Session = Depends(get_db)):
             detail=f"Blog with the id {id} is not available!",
         )
     return blog
+
+
+@app.post("/user")
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name, email=request.email, password=request.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
