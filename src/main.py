f4a9f8da883a3e46db7012a363d81254ed68a0ac
jrpespinas <jrpespinas@gmail.com>
2021-04-27 14:47:20 +0800

    feat: add get user by id method

--- src/main.py
+++ src/main.py
@@ -71,7 +71,7 @@ def get_blog_by_id(id: int, db: Session = Depends(get_db)):
 
     if not blog:
         raise HTTPException(
-            status_code=status.HTTP_201_CREATED,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Blog with the id {id} is not available!",
         )
     return blog
@@ -88,3 +88,16 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/user/{id}", response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with the id {id} is not available!",
+        )
+
+    return user
