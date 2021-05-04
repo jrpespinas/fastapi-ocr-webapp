944e77ca710cb27dd021aeb875844284c93ae4a8
Bogs Espinas <jrpespinas@gmail.com>
2021-05-04 23:07:28 +0800

    feat: add method to verify passwords

--- src/security.py
+++ src/security.py
@@ -7,3 +7,7 @@ class Hashing:
     @staticmethod
     def get_hash_password(password: str):
         return pwd_cxt.hash(password)
+
+    @staticmethod
+    def verify_password(user_password: str, request_password: str):
+        return pwd_cxt.verify(request_password, user_password)
