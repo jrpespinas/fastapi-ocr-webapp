b820cfc4fbd28e12d1c315eb21904432bc02f89c
Bogs Espinas <jrpespinas@gmail.com>
2021-05-14 15:55:54 +0800

    feat: add authenticator to users

--- src/routers/user.py
+++ src/routers/user.py
@@ -6,32 +6,53 @@ from sqlalchemy.orm import Session
 from typing import List
 from .. import schemas, database
 from ..repository import user
+from . import oauth2
 
 router = APIRouter(prefix="/user", tags=["Users"])
 
 
 @router.post("/", response_model=schemas.ShowUser)
-def create_user(request: schemas.User, db: Session = Depends(database.get_db)):
+def create_user(
+    request: schemas.User,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return user.create_user(request, db)
 
 
 @router.get("/", response_model=List[schemas.ShowUser])
-def get_all_users(db: Session = Depends(database.get_db)):
+def get_all_users(
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return user.get_all(db)
 
 
 @router.get("/{id}", response_model=schemas.ShowUser)
-def get_user(id: int, db: Session = Depends(database.get_db)):
+def get_user(
+    id: int,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return user.get_user_by_id(id, db)
 
 
 @router.delete("/{id}")
-def delete_user(id: int, db: Session = Depends(database.get_db)):
+def delete_user(
+    id: int,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return user.delete_user_by_id(id, db)
 
 
 @router.put(
     "/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.ShowSingleUser
 )
-def update_user(id: int, request: schemas.User, db: Session = Depends(database.get_db)):
+def update_user(
+    id: int,
+    request: schemas.User,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return user.update_user_by_id(id, request, db)
