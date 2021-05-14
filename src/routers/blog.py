80a26142aead7b974de6454d499cfbfdc2a825ed
Bogs Espinas <jrpespinas@gmail.com>
2021-05-14 15:52:34 +0800

    feat: add oauth2 to blog parameters

--- src/routers/blog.py
+++ src/routers/blog.py
@@ -3,33 +3,54 @@ from sqlalchemy.orm import Session
 from typing import List
 from .. import schemas, database
 from ..repository import blog
+from . import oauth2
 
 
 router = APIRouter(prefix="/blog", tags=["Blogs"])
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
+def create(
+    request: schemas.Blog,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return blog.create_blog(request, db)
 
 
 @router.get("/", response_model=List[schemas.ShowBlog])
-def get_blogs(db: Session = Depends(database.get_db)):
+def get_blogs(
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return blog.get_all(db)
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def get_blog_by_id(id: int, db: Session = Depends(database.get_db)):
+def get_blog_by_id(
+    id: int,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return blog.get_blog_by_id(id, db)
 
 
 @router.put(
     "/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.ShowSingleBlog
 )
-def update(id: int, request: schemas.Blog, db: Session = Depends(database.get_db)):
+def update(
+    id: int,
+    request: schemas.Blog,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return blog.update_blog_by_id(id, request, db)
 
 
 @router.delete("/{id}")
-def destroy(id: int, db: Session = Depends(database.get_db)):
+def destroy(
+    id: int,
+    db: Session = Depends(database.get_db),
+    current_user: schemas.User = Depends(oauth2.get_current_user),
+):
     return blog.delete_blog_by_id(id, db)
